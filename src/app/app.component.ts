7406f00c80920585887a37127d7e15e26aa8a9c3
Minh <nguyenngocminh190901@gmail.com>
2025-05-04 18:38:03 +0700

    update theme

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AuthService } from '@shared/data-access/auth.service';
 import { MeService } from '@shared/data-access/me.service';
+import { ThemeService } from '@shared/data-access/theme.service';
 
 @Component({
   selector: 'app-root',
@@ -14,11 +15,17 @@ export class AppComponent {
   private authService = inject(AuthService);
   meService = inject(MeService);
 
+  private _themeService = inject(ThemeService);
+
   constructor() {
     this.authService.restoreSession();
 
     setTimeout(() => {
       console.log(this.meService.info());
     }, 2000);
+  }
+
+  toggleTheme(): void {
+    this._themeService.toggleDarkMode();
   }
 }

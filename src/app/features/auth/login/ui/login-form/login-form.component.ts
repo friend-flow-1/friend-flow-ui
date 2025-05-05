e2115dadc51161a404c628a85d65da1846ead8e5
Minh <nguyenngocminh190901@gmail.com>
2025-05-06 00:36:40 +0700

    update register, login page

--- src/app/features/auth/login/ui/login-form/login-form.component.ts
+++ src/app/features/auth/login/ui/login-form/login-form.component.ts
@@ -6,14 +6,18 @@ import { AuthFormService } from '@features/auth/services/auth-form.service';
 import { HlmButtonDirective } from '@shared/ui/ui-button-helm/src';
 import { HlmInputDirective } from '@shared/ui/ui-input-helm/src';
 import { HlmLabelDirective } from '@shared/ui/ui-label-helm/src';
+import { HlmFormFieldModule } from '@shared/ui/ui-formfield-helm/src';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login-form',
   imports: [
     ReactiveFormsModule,
+    HlmFormFieldModule,
     HlmButtonDirective,
     HlmInputDirective,
     HlmLabelDirective,
+    RouterLink,
   ],
   templateUrl: './login-form.component.html',
   styleUrl: './login-form.component.scss',
--- src/app/features/auth/register/ui/register-form/register-form.component.ts
+++ src/app/features/auth/register/ui/register-form/register-form.component.ts
@@ -3,12 +3,25 @@ import { RegisterStatus } from '../../data-access/register.service';
 import { RegisterCredentials } from '@shared/interfaces';
 import { AuthService } from '@shared/data-access/auth.service';
 import { AuthFormService } from '@features/auth/services/auth-form.service';
-import { Router } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
+import { HlmFormFieldModule } from '@shared/ui/ui-formfield-helm/src';
+import { HlmInputDirective } from '@shared/ui/ui-input-helm/src';
+import { HlmLabelDirective } from '@shared/ui/ui-label-helm/src';
+import { BrnSelectImports } from '@spartan-ng/brain/select';
+import { HlmSelectImports } from '@shared/ui/ui-select-helm/src';
 
 @Component({
   selector: 'app-register-form',
-  imports: [ReactiveFormsModule],
+  imports: [
+    RouterLink,
+    ReactiveFormsModule,
+    HlmFormFieldModule,
+    HlmInputDirective,
+    HlmLabelDirective,
+    BrnSelectImports,
+    HlmSelectImports,
+  ],
   templateUrl: './register-form.component.html',
   styleUrl: './register-form.component.scss',
 })

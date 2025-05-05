dacc9d38adb64afb5fe12035b74ebe3ce1592590
Minh <nguyenngocminh190901@gmail.com>
2025-05-05 21:01:00 +0700

    update

--- src/app/features/auth/login/ui/login-form/login-form.component.ts
+++ src/app/features/auth/login/ui/login-form/login-form.component.ts
@@ -3,12 +3,18 @@ import { LoginStatus } from '../../data-access/login.service';
 import { Credentials } from '@shared/interfaces';
 import { ReactiveFormsModule } from '@angular/forms';
 import { AuthFormService } from '@features/auth/services/auth-form.service';
-import { HlmButtonModule } from '@shared/ui/ui-button-helm/src';
-import { HlmInputModule } from '@shared/ui/ui-input-helm/src';
+import { HlmButtonDirective } from '@shared/ui/ui-button-helm/src';
+import { HlmInputDirective } from '@shared/ui/ui-input-helm/src';
+import { HlmLabelDirective } from '@shared/ui/ui-label-helm/src';
 
 @Component({
   selector: 'app-login-form',
-  imports: [ReactiveFormsModule, HlmButtonModule, HlmInputModule],
+  imports: [
+    ReactiveFormsModule,
+    HlmButtonDirective,
+    HlmInputDirective,
+    HlmLabelDirective,
+  ],
   templateUrl: './login-form.component.html',
   styleUrl: './login-form.component.scss',
 })

a738b1c6cd525cef5ad070e10f78739e8d218991
Minh <nguyenngocminh190901@gmail.com>
2025-05-09 01:20:21 +0700

    chat layout

--- src/app/features/chat/chat.routes.ts
+++ src/app/features/chat/chat.routes.ts
@@ -2,19 +2,26 @@ import { Routes } from '@angular/router';
 import { AuthGuard } from '@core/guards/auth.guard';
 import { MeChannelComponent } from './me/me-channel/me-channel.component';
 import { ChannelComponent } from './channel/channel.component';
+import { ChatLayoutComponent } from 'app/layouts/chat-layout/chat-layout.component';
 
 export const chatRoutes: Routes = [
   {
-    path: 'channels/@me',
-    component: MeChannelComponent,
-  },
-  {
-    path: 'channels/:id',
-    component: ChannelComponent,
-  },
-  {
     path: '',
-    redirectTo: 'channels/@me',
-    pathMatch: 'full',
+    component: ChatLayoutComponent,
+    children: [
+      {
+        path: 'channels/@me',
+        component: MeChannelComponent,
+      },
+      {
+        path: 'channels/:id',
+        component: ChannelComponent,
+      },
+      {
+        path: '',
+        redirectTo: 'channels/@me',
+        pathMatch: 'full',
+      },
+    ],
   },
 ];
